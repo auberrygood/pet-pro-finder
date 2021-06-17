5765010c6492f052cd7951de7294870403e86d6d
Aubrey Sanders <aubrey.sandrs@gmail.com>
2021-06-17 22:41:53 +0000

    added html files to start working on front end; focused on grooming profession and able to get basic tasks working--submit you want to search for groomer, goes to groomer search form, fill out form and submit, goes to results page, results are clickable and take you to result details

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db
 import crud
+import os
+import requests
 
 from jinja2 import StrictUndefined
 
@@ -11,17 +13,71 @@ app.secret_key = "pet"
 app.jinja_env.undefined = StrictUndefined
 
 
-@app.route("/")
+YELP_KEY = os.environ['YELP_KEY']
+
+
+@app.route('/')
 def homepage():
     """View homepage."""
 
     return render_template("homepage.html")
 
 
+@app.route('/groomer')
+def show_groomer_search_form():
+    """Show groomer search form"""
+
+    return render_template('groomer-search-form.html')
 
 
+@app.route('/groomer/search')
+def find_groomers():
+    """Search for groomers"""
+
+    term = 'Pet Groomers'
+    location = request.args.get('address', '')
+    radius = request.args.get('radius', '')
+    sort_by = request.args.get('sort_by','')
+
+    #converting user's miles to yelp's meters
+    radius = int(radius) * 1609
+
+    url = 'https://api.yelp.com/v3/businesses/search'
+    payload = {'Authorization': f'bearer {YELP_KEY}'}
+    
+    parameters = {'term': term,
+                'limit': 10,
+                'location': location,
+                'radius': radius,
+                'sort_by': sort_by}
+
+    response = requests.get(url, params=parameters, headers=payload)
+    data = response.json()
+
+    groomers = data['businesses'] #list of business objects
+
+    return render_template ('search-results.html',
+                            data=data,
+                            professionals=groomers,
+                            term=term)
 
 
+@app.route('/professional/<id>')
+def get_professional_details(id):
+    """View the details of a professional."""
+
+
+    url = f'https://api.yelp.com/v3/businesses/{id}' #implement f string to register {id} as variable
+    payload = {'Authorization': f'bearer {YELP_KEY}'}
+
+    response = requests.get(url, headers=payload)
+    professional = response.json()
+    
+    categories = professional['categories']
+
+    return render_template('professional-details.html',
+                            professional=professional,
+                            categories=categories)
 
 
 
